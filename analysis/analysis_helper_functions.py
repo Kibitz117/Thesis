4968fd746c6650926eefb1e9167bc291c87502da
Jacob Cole Barcelona <46933473+Kibitz117@users.noreply.github.com>
2023-10-29 13:48:45 -0500

    Testing regressors for xgb and rf

--- analysis/analysis_helper_functions.py
+++ analysis/analysis_helper_functions.py
@@ -2,15 +2,24 @@ from math import sqrt
 import pandas as pd
 import numpy as np
 # Function to calculate portfolio returns
-def calculate_portfolio_returns(portfolios, returns_df):
+def calculate_portfolio_returns(portfolios, returns_df, position_type='long'):
+    # Merging the portfolios DataFrame with the returns DataFrame on 'date' and 'TICKER'
     merged_df = portfolios.merge(returns_df, on=['date', 'TICKER'])
-    portfolio_returns = merged_df['RET'].groupby(merged_df['date']).sum()
+    
+    # Calculating the return based on the position type
+    if position_type == 'short':
+        merged_df['weighted_RET'] = -merged_df['RET']
+    else:  # Default to long position if anything other than 'short' is specified
+        merged_df['weighted_RET'] = merged_df['RET']
+    
+    # Grouping by date and summing the weighted returns to get the portfolio return for each date
+    portfolio_returns = merged_df.groupby('date')['weighted_RET'].sum()
     
     return portfolio_returns
 
 
 # Function to calculate Sharpe ratio
-def calculate_sharpe_ratio(returns, rf=0.02):
+def calculate_sharpe_ratio(returns, rf=0.004):
     risk_free_rate = 0.02
     daily_risk_free_return = risk_free_rate/252
 
